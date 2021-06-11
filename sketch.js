cd3978ef42e234b8e7e6bdfafb608f0d53435b6f
Upasyo Kushari <69452530+upasyo@users.noreply.github.com>
2021-06-11 10:46:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
-var starImg,bgImg;
-var star, starBody;
-//create variable for fairy sprite and fairyImg
-var fairy,fairy_Img;
-var joy_sound;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var starImg,bgImg;
+var star, starBody;
+//create variable for fairy sprite and fairyImg
+var fairy,fairy_Img;
+var joy_sound;
+
+
 function preload()
 {
-	starImg = loadImage("star.png");
-	bgImg = loadImage("starNight.png");
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
 	//load animation for fairy here
-    fairy_Img = loadAnimation("fairyimage1.png","fairyimage2.png");
-	joy_sound = loadSound("JoyMusic.mp3");
+    fairy_Img = loadAnimation("images/fairyimage1.png","images/fairyimage2.png");
+	joy_sound = loadSound("sound/JoyMusic.mp3");
 }
 
 function setup() {
@@ -23,36 +25,36 @@ function setup() {
 	//write code to play fairyVoice sound
 
 	//create fairy sprite and add animation for fairy
-       joy_sound.play();
+
 
 	star = createSprite(width,30);
 	star.addImage(starImg);
 	star.scale = 0.2;
-  
+    star.debug=false;
 	star.setCollider("circle",10,20,80);
 	fairy = createSprite(width/6,360.4);
 	fairy.addAnimation("fairy_dancing",fairy_Img);
 	fairy.scale=0.2;
-        
+
 	engine = Engine.create();
 	world = engine.world;
 
 	starBody = Bodies.circle(width/1.5 , 110 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, starBody);
 	
-	
+	Engine.run(engine);
+
 }
 
 
 function draw() {
   background(bgImg);
 
-  Engine.run(engine);
-
+  joy_sound.play();
   star.x= starBody.position.x 
   star.y= starBody.position.y 
   
-  console.log(star.x);
+  console.log(star.y);
 
   //write code to stop star in the hand of fairy
 if(star.y> 200 && starBody.position.y >=330){
